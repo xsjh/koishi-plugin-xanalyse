869dbf969d93fdbc39122e5e1f04fe172326ac7a
xsjh <437831752@qq.com>
2025-04-28 11:31:47 +0800

    更换镜像站，成功获取推文截图和推文文字

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,84 @@ export const Config: Schema<Config> = Schema.object({
 });
 
 export function apply(ctx: Context) {
-  ctx.command('twitter', '获取x截图')
-    .action(async ({session}) => {
+  ctx.command('twitter [...arg]', '获取x截图')
+    .action(async ({session}, ...arg) => {
       try {
-        const page = await ctx.puppeteer.page();
-        await page.setViewport({ width: 1200, height: 800 });
-        const tweetUrl = 'https://x.com/home'; 
-        await page.goto(tweetUrl, { waitUntil: 'networkidle0' });
-        const buffer:any = await page.screenshot({ type: "png" });
-        await session.send(h.image(buffer));
-        await session.send("1111");
+        const url = arg.join(' ').trim();
+        if (url == ''){
+        await session.send("您输入的url为空");
+      }else{
+        const tweetContent = await getTweetContent(ctx.puppeteer, url);
+        console.log("获取的图片url", tweetContent.imgUrls);
+        const msg = `获取成功：
+${tweetContent.word_content}
+${h.image(tweetContent.buffer)}       
+          `
+        await session.send(msg);
+        
+      }
       } catch (error) {
-        console.log('获取截图失败', error);
+        console.log("获取推文过程失败");
       }
     });
+
 }
+
+async function getTweetContent(pptr,url) {
+  try {
+    const page = await pptr.page();// 初始化浏览器
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36");
+    const tweetUrl = `${url}`; // 加载url并获取元素
+    await page.goto(tweetUrl, { waitUntil: 'networkidle0' });
+
+    // 1、获取推文文字内容
+    const element = await page.$('div.timeline-item');
+    if (!element) {
+      throw new Error('未能找到指定的元素');
+    }
+    const word_content = await page.evaluate(() => {
+      const element = document.querySelector('div.tweet-content.media-body');
+      if (!element) return '';
+      let textContent = element.textContent || '';
+      return textContent.trim();});
+
+    // 2、获取推文文字内容
+    const buffer = await element.screenshot({ type: "webp" });// 获取完整截图  
+    
+    // 3、获取推文图片
+    const imgUrls = await page.evaluate(() => {
+      const imgelement = document.querySelector('div.gallery-row');
+      if (!imgelement) return [];
+      
+      
+      
+    });
+    // 返回图片与文字
+    return {
+      imgUrls,
+      word_content,
+      buffer
+    }        
+  } catch (error) {
+    console.log("加载页面或元素失效，请检查UserAgent", error);
+    return {
+      imgUrls: [],
+      word_content: '',
+      buffer: null
+    };
+  }
+}
+
+
+
+
+
+      
+      
+
+      
+
+    
+    
+
+ 
